ff47961cfe3b7e3f8e83f8550656ffa4c79a0e4a
ReanesX <miha200273@gmail.com>
2020-10-02 18:26:20 +0500

    Добавьте файлы проекта.

---
+++ FunctionValue/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace FunctionValue
+{
+    class Program
+    {
+        static float IsFunc(float x)
+        {
+            float y = x * x;
+            return y;
+        }
+        static void Main(string[] args)
+        {
+            Console.Write("Введите шаг построения: ");
+            float step = float.Parse(Console.ReadLine());
+
+            Console.Write("Введите начальное значение дипозона X: ");
+            float startValue = float.Parse(Console.ReadLine());
+
+            Console.Write("Введите конечное значение дипозона X: ");
+            float endValue = float.Parse(Console.ReadLine());
+            
+            Console.WriteLine("|---------------|---------------|");
+            Console.WriteLine("|\tx\t|\ty\t|");
+            Console.WriteLine("|---------------|---------------|");
+           
+            for (float x = startValue; x <= endValue; x += step)
+            {
+                float y = IsFunc(x);
+                Console.WriteLine($"|\t{x}\t|\t{y}\t|");
+            }
+        }
+    }
+}
